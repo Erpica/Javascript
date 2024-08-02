bd11016092066ea0c21f2d52602b64fe7ad144bb
Erpica <160789038+Erpica@users.noreply.github.com>
2024-08-02 16:53:25 +0200

    maas

--- Basic/16-funciones.js
+++ Basic/16-funciones.js
@@ -1,4 +1,4 @@
-//log es una funcion de consola
+//log es una funcion de consola (como has, get, keys, ...)
 
 function myFunction(){
     console.log("¡Hola, mi función!")
@@ -13,7 +13,7 @@ function myFunctionConParametros(name){
 myFunctionConParametros("Pica")
 myFunctionConParametros("Anto")
 
-// Funciones anónimas
+// Funciones anónimas: hay que asignarla a una variable o a una constante
 const myFunc2 = function (name){
     console.log(`¡Hola, ${name}!`)
 }
@@ -21,9 +21,27 @@ const myFunc2 = function (name){
 myFunc2 ("Er Pica")
 
 // arrow functions: una forma mas concisa de escribir funciones (de la epoca de quitar el punto y coma)
+// hay que asignarla a una variable o a una constante
+// en este caso se puede utilizar porque solo quiero que reciba un parámetro: name
 const myFunc3 = (name) => {
     console.log(`¡Hola, ${name}!`)
 }
 
 const myFunc4 = (name) => console.log(`¡Hola, ${name}!`)
 myFunc4("hola hola hola")
+
+function defaultSum (a=0, b=0){ //podemos inicializar los parámetros para que no de error si me la mandan sin parámetros
+    console.log(a + b)
+}
+defaultSum()
+defaultSum(4)
+defaultSum(4,5)
+
+function mult(a,b){
+    return a*b
+}
+
+console.log(mult(5,10))
+
+
+
